1dfbe85a5a454a11300d4e5a8732052679e6643e
danielagon <daniela.gonzalezramirez@hotmail.com>
2018-08-27 21:33:12 -0500

    Completo

--- src/main/java/edu/eci/arsw/myrestaurant/restcontrollers/OrdersAPIController.java
+++ src/main/java/edu/eci/arsw/myrestaurant/restcontrollers/OrdersAPIController.java
@@ -16,15 +16,24 @@
  */
 package edu.eci.arsw.myrestaurant.restcontrollers;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import edu.eci.arsw.myrestaurant.model.Order;
 import edu.eci.arsw.myrestaurant.model.ProductType;
 import edu.eci.arsw.myrestaurant.model.RestaurantProduct;
+import edu.eci.arsw.myrestaurant.services.OrderServicesException;
+import edu.eci.arsw.myrestaurant.services.RestaurantOrderServices;
 import edu.eci.arsw.myrestaurant.services.RestaurantOrderServicesStub;
-import java.util.Hashtable;
+import java.io.IOException;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -34,7 +43,95 @@ import org.springframework.web.bind.annotation.RestController;
  *
  * @author hcadavid
  */
-public class OrdersAPIController {
 
+@Service
+@RestController
+@RequestMapping(value = "/orders")
+public class OrdersAPIController {
     
+    @Autowired
+    private RestaurantOrderServices orders;
+    
+    @RequestMapping(method = RequestMethod.GET)
+    public ResponseEntity<?> manejadorGetRecursoOrders(){
+        try{
+            ObjectMapper mapper = new ObjectMapper();
+            String json = mapper.writeValueAsString(orders);
+            return new ResponseEntity<>(json,HttpStatus.ACCEPTED);
+        }catch (JsonProcessingException ex){
+            Logger.getLogger(OrdersAPIController.class.getName()).log(Level.SEVERE, null, ex);
+            return new ResponseEntity<>("Error creando el json",HttpStatus.NO_CONTENT);
+        }
+    }
+    
+    @RequestMapping(value = "/{idmesa}")
+    public ResponseEntity<?> manejadorGetOrder(@PathVariable int idmesa){
+        try{
+            Order order = orders.getTableOrder(idmesa);
+            ObjectMapper mapper = new ObjectMapper();
+            String json = mapper.writeValueAsString(order);
+            return new ResponseEntity<>(json,HttpStatus.ACCEPTED);
+        }catch(NullPointerException ex){
+            Logger.getLogger(OrdersAPIController.class.getName()).log(Level.SEVERE, null, ex);
+            return new ResponseEntity<>("El numero de la mesa no existe o no tiene una orden asociada",HttpStatus.NOT_FOUND);
+        }catch(JsonProcessingException ex){
+            Logger.getLogger(OrdersAPIController.class.getName()).log(Level.SEVERE, null, ex);
+            return new ResponseEntity<>("Error creando el json",HttpStatus.NO_CONTENT);
+        }
+    }
+    
+    @RequestMapping(method = RequestMethod.POST)	
+    public ResponseEntity<?> manejadorPostRecursoOrders(@RequestBody String o){
+        try {
+            //registrar dato
+            ObjectMapper mapper = new ObjectMapper();
+            Order newOrder = mapper.readValue(o, Order.class);
+            orders.addNewOrderToTable(newOrder);
+            return new ResponseEntity<>(HttpStatus.CREATED);           
+	} catch (OrderServicesException ex) {        
+            Logger.getLogger(OrdersAPIController.class.getName()).log(Level.SEVERE, null, ex);
+            return new ResponseEntity<>("Error en la nueva orden",HttpStatus.FORBIDDEN);
+        } catch (IOException ex) {
+            Logger.getLogger(OrdersAPIController.class.getName()).log(Level.SEVERE, null, ex);
+            return new ResponseEntity<>("Error al crear la nueva orden",HttpStatus.FORBIDDEN);
+        }        
+    }
+    
+    @RequestMapping(value = "/{idmesa}/total")
+    public ResponseEntity<?> manejadorGetTotal(@PathVariable int idmesa){
+        try{
+            int total = orders.calculateTableBill(idmesa);
+            return new ResponseEntity<>(total,HttpStatus.ACCEPTED);
+        }catch (OrderServicesException ex){
+            Logger.getLogger(OrdersAPIController.class.getName()).log(Level.SEVERE, null, ex);
+            return new ResponseEntity<>("Error en la orden",HttpStatus.NOT_FOUND);
+        }
+    }
+    
+    @RequestMapping(value = "/{idmesa}",method = RequestMethod.PUT)
+    public ResponseEntity<?> manejadorPutRecursoOrders(@RequestBody String p, @PathVariable int idmesa){
+        try{
+            ObjectMapper mapper = new ObjectMapper();
+            Map<String,Integer> products = mapper.readValue(p, ConcurrentHashMap.class);
+            Order order = orders.getTableOrder(idmesa);
+            for (String i:products.keySet()){
+                order.addDish(i, products.get(i));
+            }
+            return new ResponseEntity<>(HttpStatus.ACCEPTED);
+        }catch (IOException ex){
+            Logger.getLogger(OrdersAPIController.class.getName()).log(Level.SEVERE, null, ex);
+            return new ResponseEntity<>("Error ingresando el nuevo producto",HttpStatus.NO_CONTENT);
+        }
+    }
+    
+    @RequestMapping(value = "/{idmesa}", method = RequestMethod.DELETE)
+    public ResponseEntity<?> manejadorDeleteOrders(@PathVariable int idmesa){
+        try{
+            orders.releaseTable(idmesa);
+            return new ResponseEntity<>(HttpStatus.ACCEPTED);
+        } catch (OrderServicesException ex) {
+            Logger.getLogger(OrdersAPIController.class.getName()).log(Level.SEVERE, null, ex);
+            return new ResponseEntity<>("El numero de la mesa no existe o no tiene una orden asociada",HttpStatus.NOT_FOUND);
+        }
+    }
 }
--- src/test/java/edu/eci/arsw/myrestaurant/test/ApplicationServicesTests.java
+++ src/test/java/edu/eci/arsw/myrestaurant/test/ApplicationServicesTests.java
@@ -7,6 +7,7 @@ import edu.eci.arsw.myrestaurant.services.RestaurantOrderServicesStub;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import org.junit.Test;
+import org.junit.Assert;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringBootConfiguration;
@@ -17,16 +18,21 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest()
 public class ApplicationServicesTests {
 
-    
+    @Autowired
     RestaurantOrderServicesStub ros;
 
     
     @Test
-    public void contextLoads() throws OrderServicesException{
+    public void contextLoads() throws OrderServicesException{        
         
-        
-        
-        
+    }
+    
+    @Test
+    public void calcularTotal() throws OrderServicesException{
+        int totalCalculado = ros.calculateTableBill(1);
+        float totalEsperado = (10000*3)+(10000*3*0.19f)+(3000)+(3000*0.19f)+(1300*4)+(1300*4*0.16f);
+        int totalCalculadoManual = (int) totalEsperado;
+        org.junit.Assert.assertEquals(totalCalculado,totalCalculadoManual);
     }
 
 }
